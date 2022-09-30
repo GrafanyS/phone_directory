6d48d08dce4b5ae09eec2507ec66e1b009f869e2
GrafanyS <admin@valet.of.by>
2022-09-30 09:48:23 +0300

    ui.py+

---
+++ ui.py
@@ -0,0 +1,7 @@
+def get_value():
+    dict = {}
+    dict['surname'] = input('input surname: ')
+    dict['name'] = input('input name: ')
+    dict['tel'] = input('input phone: ')
+    dict['comment'] = input('input description: ')
+    return dict
