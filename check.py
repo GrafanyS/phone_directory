82d01a5cead4fe5fa922efbaf37e821d2a4be634
Artur <artur.gikramin@yandex.ru>
2022-10-02 23:23:19 +0500

    check.py + 1

--- check.py
+++ check.py
@@ -6,12 +6,15 @@ print(Style.DIM + 'and in dim text')
 print(Style.RESET_ALL)
 print('back to normal now')
 
-print(Blue.YELLOW+'Такого пункта меню нет! Попробуйте снова.')
+print(Blue.YELLOW + 'Такого пункта меню нет! Попробуйте снова.')
+
+
 def check_main_menu():
     while True:
         try:
             num = int(input('Введите номер пункта, который хотите выполнить: '))
-            if 0 <= num <= 3: break
+            if 0 <= num <= 3:
+                break
             else:
                 print('Такого пункта меню нет!')
                 print("\033[31m {}\033[0m".format('Такого пункта меню нет! Попробуйте снова.'))
