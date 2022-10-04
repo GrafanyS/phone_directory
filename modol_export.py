0e6b13fc8302c5d1e6c9927c035114d75bc062d0
GrafanyS <admin@valet.of.by>
2022-10-04 17:08:20 +0300

    импорт экспорт db

--- modol_export.py
+++ modol_export.py
@@ -13,8 +13,10 @@ def export():
         file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
         file_writer.writerow(["Фамилия\t", "Имя\t", "Телефон\t", "Описание\t"])
         for i in phone_dir:
-            file_writer.writerow([i['surname'], i['name'], i['tel'], i['comment']])
+            file_writer.writerow([i['id']],[i['surname'], i['name'], i['tel'], i['comment']])
             count += 1
     print(Fore.BLACK + "" + Back.GREEN + f"Экспорт завершен успешно. "
                                          f'Всего экспортировано {count} контактов.' + Style.RESET_ALL)
 
+
+export()
--- modol_import.py
+++ modol_import.py
@@ -3,11 +3,13 @@ import os
 import csv
 import json
 from db_link import *
-
+from colorama import Fore, Back, Style
+import time
+import pandas as pd
 
 def import_csv():
     result = []
-    with open("file_for_import.csv", encoding='utf-8') as r_file:
+    with open(csvFilename, encoding='utf-8') as r_file:
         file_reader = csv.reader(r_file, delimiter=",")
         count = 0
         for row in file_reader:
@@ -20,9 +22,50 @@ def import_csv():
                 result.append(temp_dict)
 
             count += 1
-    with open('data_base.json', 'w') as file:
+    with open(jsonFilename, 'w', encoding='utf-8') as file:
         json.dump(result, file, indent=2)
 
-    print(f'\033[30m\033[42m\033[4m Импорт завершен успешно. '
-          f'Всего импортировано {count - 1} контактов. \033[0m')
+    print(Fore.BLACK + "" + Back.GREEN + f'Импорт завершен успешно. '
+          f'Всего импортировано {count - 1} контактов.' + Style.RESET_ALL)
 
+
+def import_json():
+    result = []
+    with open(csvFilename, "r", encoding='utf-8') as r_file:
+        file_reader = csv.reader(r_file, delimiter=",")
+        count = 0
+        for row in file_reader:
+            if count == 0:
+                count += 1
+                continue
+
+            else:
+                temp_dict = {
+                    'surname': row[0], 'name': row[1], 'tel': row[2], 'comment': row[3]}
+                result.append(temp_dict)
+
+            count += 1
+    with open(jsonFilename, 'w', encoding='utf-8') as file:
+        json.dump(result, file, indent=2)
+
+    print(Fore.BLACK + "" + Back.GREEN + f'Импорт завершен успешно. '
+          f'Всего импортировано {count - 1} контактов.' + Style.RESET_ALL)
+
+
+
+
+
+
+
+
+def convcsv():
+    with open(jsonFilename, 'r', encoding='utf-8') as inputfile:
+        df = pd.read_json(inputfile)
+
+    df.to_csv(csvFile, encoding='utf-8', index=False)
+
+start = time.perf_counter()
+convcsv()
+finish = time.perf_counter()
+print(Fore.BLACK + "" + Back.GREEN + f'Импорт завершен успешно. '
+      f'Всего импортировано {finish - start:0.4f} контактов.' + Style.RESET_ALL)
