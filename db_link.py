1e598414f0f95276f674a7aa567f4bf606b0b5c1
GrafanyS <admin@valet.of.by>
2022-10-04 09:44:00 +0300

    db_link.py Андрей

---
+++ db_link.py
@@ -0,0 +1,4 @@
+csvFilename = r'DB_Directory\phone_directory.csv'
+jsonFilename = r'DB_Directory\phone_directory.json'
+csvFile = r'DB_export\phone_directory_export.csv'
+jsonFile = r'DB_export\phone_directory_export.json'
