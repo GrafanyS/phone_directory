b558366eb972743601194b6791b390fac2bfde7b
GrafanyS <admin@valet.of.by>
2022-10-05 12:25:02 +0300

    db_link.py готовый grafany

--- db_link.py
+++ db_link.py
@@ -1,5 +1,21 @@
-csvFilename = r'DB_Directory/phone_directory.csv'
-jsonFilename = r'DB_Directory/phone_directory.json'
-csvFile = r'DB_export/phone_directory_export.csv'
-jsonFile = r'DB_export/phone_directory_export.json'
-LOG = r'Logger/logger.log'
+from sys import platform
+
+
+if platform == "linux" or platform == "linux2":
+    csvFilename = r'DB_Directory/phone_directory.csv'
+    jsonFilename = r'DB_Directory/phone_directory.json'
+    csvFile = r'DB_export/phone_directory_export.csv'
+    jsonFile = r'DB_export/phone_directory_export.json'
+    LOG = r'Logger/logger.log'
+elif platform == "darwin":
+    csvFilename = r'DB_Directory/phone_directory.csv'
+    jsonFilename = r'DB_Directory/phone_directory.json'
+    csvFile = r'DB_export/phone_directory_export.csv'
+    jsonFile = r'DB_export/phone_directory_export.json'
+    LOG = r'Logger/logger.log'
+elif platform == "win32":
+    csvFilename = r'DB_Directory\phone_directory.csv'
+    jsonFilename = r'DB_Directory\phone_directory.json'
+    csvFile = r'DB_export\phone_directory_export.csv'
+    jsonFile = r'DB_export\phone_directory_export.json'
+    LOG = r'Logger\logger.log'
