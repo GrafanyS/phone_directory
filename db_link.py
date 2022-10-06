619cb8e8486c4bb521fab57a3912e5f7cb7a1a6c
GrafanyS <admin@valet.of.by>
2022-10-06 14:37:02 +0300

    db_link.py готовый вариант

--- db_link.py
+++ db_link.py
@@ -1,21 +1,28 @@
+from distutils.text_file import TextFile
 from sys import platform
 
 
 if platform == "linux" or platform == "linux2":
     csvFilename = r'DB_Directory/phone_directory.csv'
     jsonFilename = r'DB_Directory/phone_directory.json'
+    textFilename = r'DB_Directory/phone_directory.txt'
     csvFile = r'DB_export/phone_directory_export.csv'
     jsonFile = r'DB_export/phone_directory_export.json'
+    textFile = r'DB_export/phone_directory_export.txt'
     LOG = r'Logger/logger.log'
 elif platform == "darwin":
     csvFilename = r'DB_Directory/phone_directory.csv'
     jsonFilename = r'DB_Directory/phone_directory.json'
+    textFilename = r'DB_Directory/phone_directory.txt'
     csvFile = r'DB_export/phone_directory_export.csv'
     jsonFile = r'DB_export/phone_directory_export.json'
+    textFile = r'DB_export/phone_directory_export.txt'
     LOG = r'Logger/logger.log'
 elif platform == "win32":
     csvFilename = r'DB_Directory\phone_directory.csv'
     jsonFilename = r'DB_Directory\phone_directory.json'
+    textFilename = r'DB_Directory\phone_directory.txt'
     csvFile = r'DB_export\phone_directory_export.csv'
     jsonFile = r'DB_export\phone_directory_export.json'
+    textFile = r'DB_export\phone_directory_export.txt'
     LOG = r'Logger\logger.log'
